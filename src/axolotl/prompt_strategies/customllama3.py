0a7722ac323a1dfecb096e685c8258834c9b98c8
xzuyn <16216325+xzuyn@users.noreply.github.com>
2024-09-13 16:52:08 -0400

    Update customllama3.py

--- src/axolotl/prompt_strategies/customllama3.py
+++ src/axolotl/prompt_strategies/customllama3.py
@@ -71,6 +71,8 @@ class CustomLLaMa3PromptTokenizingStrategy(PromptTokenizingStrategy):
             elif sharegpt_from == "gpt-chat":
                 role_name = "assistant"
                 sharegpt_value = f"{turn['name'].strip()}: {sharegpt_value}"
+            elif sharegpt_from == "thought":
+                role_name = "thought"
             else:
                 LOG.warning(f"'from' contains an unhandled string: {sharegpt_from}")
                 exit()
@@ -106,6 +108,7 @@ class CustomLLaMa3PromptTokenizingStrategy(PromptTokenizingStrategy):
                 and (
                     sharegpt_from == "gpt"
                     or sharegpt_from == "gpt-chat"
+                    or sharegpt_from == "thought"
                 )
             ):
                 labels = (
