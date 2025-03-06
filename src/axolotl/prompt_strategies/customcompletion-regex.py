f02d499deef228fd97f90b24ce0b342f211668c1
xzuyn <16216325+xzuyn@users.noreply.github.com>
2025-03-05 19:16:33 -0500

    Update customcompletion-regex.py

--- src/axolotl/prompt_strategies/customcompletion-regex.py
+++ src/axolotl/prompt_strategies/customcompletion-regex.py
@@ -3,16 +3,14 @@
 # Import necessary modules and functions
 import re
 import ftfy
-import copy
 import logging
-from typing import List, Tuple, Pattern, Dict, Union
 
 # Import from axolotl package
 from axolotl.prompt_tokenizers import PromptTokenizingStrategy
 try:
     from axolotl.prompt_strategies.formatter_regex import COMPILED_REGEX_PATTERNS
 except ImportError:
-    print("You need https://github.com/xzuyn/axolotl/blob/prompt_formats/src/axolotl/prompt_strategies/formatter_regex.py")
+    raise ImportError("You need https://github.com/xzuyn/axolotl/blob/prompt_formats/src/axolotl/prompt_strategies/formatter_regex.py")
 
 
 # Set up logging
