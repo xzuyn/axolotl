94feb4ff64acf482c0255c798e3db972a02362c0
xzuyn <16216325+xzuyn@users.noreply.github.com>
2025-05-07 15:23:29 -0400

    Add files via upload

--- src/axolotl/prompt_strategies/customchatml.py
+++ src/axolotl/prompt_strategies/customchatml.py
@@ -91,7 +91,7 @@ class CustomChatMLPromptTokenizingStrategy(PromptTokenizingStrategy):
         # Add missing BOS token
         if self.tokenizer.bos_token_id and input_ids[0] != self.tokenizer.bos_token_id:
             input_ids.insert(0, self.tokenizer.bos_token_id)
-            attention_mask.insert(0, 0)
+            attention_mask.insert(0, 1)
             labels.insert(0, IGNORE_TOKEN_ID)
 
         # Add missing EOS token
