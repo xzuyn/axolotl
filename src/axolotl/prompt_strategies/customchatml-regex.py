99749052d857e88e151edd1532579126c0f67e5c
xzuyn <16216325+xzuyn@users.noreply.github.com>
2025-05-09 06:11:00 -0400

    Update customchatml-regex.py

--- src/axolotl/prompt_strategies/customchatml-regex.py
+++ src/axolotl/prompt_strategies/customchatml-regex.py
@@ -85,7 +85,7 @@ class CustomChatMLPromptTokenizingStrategy(PromptTokenizingStrategy):
                 sharegpt_value = turn["value"].strip()
 
             # Get tokens which will be masked out if using train_on_inputs: false
-            prefix_text = f"{'\n' if i != 0 else ''}<|im_start|>{role_dict[turn['from']]}\n"
+            prefix_text = ("\n" if i != 0 else "") + f"<|im_start|>{role_dict[turn['from']]}\n"
             tokenized_prefix_text = self.tokenizer(
                 text=prefix_text,
                 add_special_tokens=False,
