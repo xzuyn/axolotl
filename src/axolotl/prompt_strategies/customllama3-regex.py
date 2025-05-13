18ebf4550ecdd0e7caa2bd9ebe571670484ae6fc
xzuyn <16216325+xzuyn@users.noreply.github.com>
2025-05-13 12:07:30 -0400

    Update customllama3-regex.py

--- src/axolotl/prompt_strategies/customllama3-regex.py
+++ src/axolotl/prompt_strategies/customllama3-regex.py
@@ -2,15 +2,19 @@
 
 # Import necessary modules and functions
 import re
-import ftfy
+try:
+    import ftfy
+except ImportError:
+    raise ImportError("You need ftfy. https://pypi.org/project/ftfy/")
 import logging
+from copy import deepcopy
 
 # Import from axolotl package
 from axolotl.prompt_tokenizers import PromptTokenizingStrategy
 try:
     from axolotl.prompt_strategies.formatter_regex import COMPILED_REGEX_PATTERNS
 except ImportError:
-    raise ImportError("You need https://github.com/xzuyn/axolotl/blob/prompt_formats/src/axolotl/prompt_strategies/formatter_regex.py")
+    raise ImportError("You need https://github.com/xzuyn/axolotl/blob/came-plus-formatters/src/axolotl/prompt_strategies/formatter_regex.py")
 
 
 # Set up logging
@@ -30,6 +34,7 @@ def mask_regex_attention_tokenizer(tokenizer, text, compiled_regex_patterns, add
         return_offsets_mapping=True,
     )
 
+    regex_mask_labels = deepcopy(tokenized_text["input_ids"])
     for pattern in compiled_regex_patterns:
         for match in pattern.finditer(text):
             found_index = match.start()
@@ -38,9 +43,9 @@ def mask_regex_attention_tokenizer(tokenizer, text, compiled_regex_patterns, add
             # Check each token's character span; if it overlaps, mask it out.
             for i, (token_start, token_end) in enumerate(tokenized_text["offset_mapping"]):
                 if token_start < end_index and token_end > found_index:
-                    tokenized_text["attention_mask"][i] = 0
+                    regex_mask_labels[i] = IGNORE_TOKEN_ID
 
-    return tokenized_text
+    return tokenized_text, regex_mask_labels
 
 
 class CustomLLaMa3PromptTokenizingStrategy(PromptTokenizingStrategy):
@@ -48,12 +53,19 @@ class CustomLLaMa3PromptTokenizingStrategy(PromptTokenizingStrategy):
     Tokenizing strategy for CustomLLaMa3.
     """
 
-    def __init__(self, prompter, tokenizer, *args, **kwargs):
+    def __init__(self, prompter, tokenizer, train_on_inputs, sequence_len, *args, **kwargs):
         # Call the superclass' constructor
-        super().__init__(prompter, tokenizer, *args, **kwargs)
+        super().__init__(
+            prompter=prompter,
+            tokenizer=tokenizer,
+            train_on_inputs=train_on_inputs,
+            sequence_len=sequence_len,
+            *args,
+            **kwargs
+        )
 
     def tokenize_prompt(self, prompt):
-        # ShareGPT-to-ChatML Dictionary
+        # ShareGPT-to-LLaMa3 Dictionary
         role_dict = {
             "system": "system",
             "human": "user",
@@ -72,64 +84,111 @@ class CustomLLaMa3PromptTokenizingStrategy(PromptTokenizingStrategy):
             exit()
 
         # Iterate over each conversation turn in the prompt
-        input_ids, attention_mask = [], []
+        turn_segments = []
         for i, turn in enumerate(prompt[conversation_name]):
-            if turn["from"] == "human-chat":
-                sharegpt_value = f"{turn['name'].strip()}: {turn['value'].strip()}"
-            elif turn["from"] == "gpt-chat":
-                sharegpt_value = f"{turn['name'].strip()}: {turn['value'].strip()}"
-            else:
-                sharegpt_value = turn["value"].strip()
+            try:
+                if turn["from"] in ["human-chat", "gpt-chat"]:
+                    sharegpt_value = f"{turn['name'].strip()}: {turn['value'].strip()}"
+                else:
+                    sharegpt_value = turn["value"].strip()
+            except AttributeError:
+                LOG.warning(f"Processed sample will return empty due to AttributeError")
+                return {
+                    "input_ids": [],
+                    "attention_mask": [],
+                    "labels": []
+                }
 
-            # Get tokens which will be masked out if using train_on_inputs: false
+            # Get string which will be masked out if using train_on_inputs: false
             prefix_text = f"<|start_header_id|>{role_dict[turn['from']]}<|end_header_id|>\n\n"
-            tokenized_prefix_text = self.tokenizer(
-                text=prefix_text,
-                add_special_tokens=False,
-                truncation=False,
-                padding=False,
-                return_tensors=None,
-            )
-            # Tokenize and create mask out undesired tokens using regex patterns
-            tokenized_text = mask_regex_attention_tokenizer(
-                tokenizer=self.tokenizer,
-                text=f"{prefix_text}{ftfy.fix_text(sharegpt_value.strip())}<|eot_id|>",
-                compiled_regex_patterns=COMPILED_REGEX_PATTERNS,
-            )
 
             # Handle masked user turn
             if self.train_on_inputs is False and turn["from"] in ["system", "human", "human-chat"]:
-                tokenized_text["attention_mask"] = [0] * len(tokenized_text["attention_mask"])
+                turn_segments.append(
+                    {
+                        "from": turn["from"],
+                        "input_ids": tokenized_text["input_ids"],
+                        "attention_mask": tokenized_text["attention_mask"],
+                        "labels": [IGNORE_TOKEN_ID] * len(regex_mask_labels),
+                    }
+                )
             # Handle partially masked model turn
-            elif self.train_on_inputs is False and turn["from"] in ["gpt", "gpt-chat", "thought"]:
-                tokenized_text["attention_mask"] = (
-                    [0] * len(tokenized_prefix_text["attention_mask"])  # Mask the prefix
-                    + tokenized_text["attention_mask"][len(tokenized_prefix_text["attention_mask"]):]
+            elif self.train_on_inputs is False and turn["from"] in ["gpt", "gpt-chat"]:
+                prefix_token_count = 0
+                for start, end in tokenized_text["offset_mapping"]:
+                    if end <= len(prefix_text):
+                        prefix_token_count += 1
+                    else:
+                        break
+
+                turn_segments.append(
+                    {
+                        "from": turn["from"],
+                        "input_ids": tokenized_text["input_ids"],
+                        "attention_mask": tokenized_text["attention_mask"],
+                        "labels": (
+                            [IGNORE_TOKEN_ID] * prefix_token_count  # Mask the prefix
+                            + regex_mask_labels[prefix_token_count:]
+                        ),
+                    }
+                )
+            # Handle unmasked turn
+            else:
+                turn_segments.append(
+                    {
+                        "from": turn["from"],
+                        "input_ids": tokenized_text["input_ids"],
+                        "attention_mask": tokenized_text["attention_mask"],
+                        "labels": regex_mask_labels,
+                    }
                 )
 
-            input_ids += tokenized_text["input_ids"]
-            attention_mask += tokenized_text["attention_mask"]
+        # Only keep turns which add up to less than sequence_len (or seq_len - 1 if bos is set)
+        current_length = 0
+        trimmed_turn_segments = []
+        for turn_segment in turn_segments:
+            turn_segment_length = len(turn_segment["input_ids"])
+            if current_length + turn_segment_length > self.sequence_len - (
+                1 if self.tokenizer.bos_token_id else 0
+            ):
+                break
+            else:
+                trimmed_turn_segments.append(turn_segment)
+                current_length += turn_segment_length
 
-        # Add missing BOS token
+        # Ensure the final turn is from gpt or gpt-chat
+        while trimmed_turn_segments and trimmed_turn_segments[-1]["from"] not in ["gpt", "gpt-chat"]:
+            trimmed_turn_segments.pop()
+
+        # Return empty if there are less than 2 turns left
+        if len(trimmed_turn_segments) < 2:
+            # LOG.warning(f"Processed sample will return empty due to not enough turns")
+            return {
+                "input_ids": [],
+                "attention_mask": [],
+                "labels": []
+            }
+
+        # Combine all the turn segments
+        input_ids, attention_mask, labels = [], [], []
+        for turn_segment in trimmed_turn_segments:
+            input_ids.extend(turn_segment["input_ids"])
+            attention_mask.extend(turn_segment["attention_mllaaask"])
+            labels.extend(turn_segment["labels"])
+
+        # Add missing BOS token if needed
         if self.tokenizer.bos_token_id and input_ids[0] != self.tokenizer.bos_token_id:
             input_ids.insert(0, self.tokenizer.bos_token_id)
-            attention_mask.insert(0, 0)
-
-        # Add missing EOS token
-        if input_ids[-1] != self.tokenizer.eos_token_id:
-            input_ids.append(self.tokenizer.eos_token_id)
-            attention_mask.append(1)
+            attention_mask.insert(0, 1)
+            labels.insert(0, IGNORE_TOKEN_ID)
 
         return {
             "input_ids": input_ids,
             "attention_mask": attention_mask,
-            "labels": [
-                label if mask == 1 else IGNORE_TOKEN_ID
-                for label, mask in zip(input_ids, attention_mask)
-            ]
+            "labels": labels
         }
 
 
 # Function to load the CustomLLaMa3PromptTokenizingStrategy
 def load(tokenizer, cfg):
-    return CustomLLaMa3PromptTokenizingStrategy(None, tokenizer, cfg.train_on_inputs)
+    return CustomLLaMa3PromptTokenizingStrategy(None, tokenizer, cfg.train_on_inputs, cfg.sequence_len)
