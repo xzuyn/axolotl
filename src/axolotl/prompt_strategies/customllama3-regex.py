6efb7f60e654854c174cc75f3be1a98bf2dcf740
xzuyn <16216325+xzuyn@users.noreply.github.com>
2025-05-23 13:58:12 -0400

    Add files via upload

--- src/axolotl/prompt_strategies/customllama3-regex.py
+++ src/axolotl/prompt_strategies/customllama3-regex.py
@@ -156,7 +156,7 @@ class CustomLLaMa3PromptTokenizingStrategy(PromptTokenizingStrategy):
         for turn_segment in turn_segments:
             turn_segment_length = len(turn_segment["input_ids"])
             if current_length + turn_segment_length > self.sequence_len - (
-                1 if self.tokenizer.bos_token_id else 0
+                1 if self.tokenizer.add_bos_token and self.tokenizer.bos_token_id else 0
             ):
                 break
             else:
