9b9fe5ad1dd9c11a1c79915d386f0cbb3b1134f6
xzuyn <16216325+xzuyn@users.noreply.github.com>
2026-01-01 02:18:49 -0500

    Add files via upload

---
+++ src/axolotl/prompt_strategies/customllama3-regex-last-only-prefill.py
@@ -0,0 +1,179 @@
+"""Module containing the CustomLLaMa3PromptTokenizingStrategy class"""
+
+# Import necessary modules and functions
+import re
+import logging
+import random
+from axolotl.prompt_tokenizers import PromptTokenizingStrategy
+
+try:
+    from axolotl.prompt_strategies.regex_attention import regex_attention_tokenizer
+except ImportError:
+    raise ImportError(
+        "You need https://github.com/xzuyn/axolotl/blob/latest-formatters/src/axolotl/prompt_strategies/regex_attention.py"
+    )
+
+
+# Set up logging
+LOG = logging.getLogger("axolotl")
+
+# Define a constant token ID to ignore
+IGNORE_TOKEN_ID = -100
+
+
+class CustomLLaMa3PromptTokenizingStrategy(PromptTokenizingStrategy):
+    """
+    Tokenizing strategy for CustomLLaMa3.
+    """
+
+    def __init__(
+        self, prompter, tokenizer, train_on_inputs, sequence_len, *args, **kwargs
+    ):
+        # Call the superclass' constructor
+        super().__init__(
+            prompter=prompter,
+            tokenizer=tokenizer,
+            train_on_inputs=train_on_inputs,
+            sequence_len=sequence_len,
+            *args,
+            **kwargs,
+        )
+
+    def tokenize_prompt(self, prompt):
+        if len(prompt["prompt"]) <= 2 or (1024 < prompt["response_average_tokens"] < 3):
+            return {"input_ids": [], "attention_mask": [], "labels": []}
+
+        # Some tokenizers don't contain this, so if it doesn't exist assume it is set to True
+        add_bos = getattr(self.tokenizer, "add_bos_token", True)
+
+        # ShareGPT-to-LLaMa3 Dictionary
+        role_dict = {
+            "system": "system",
+            "user": "user",
+            "human": "user",
+            "human-chat": "user",
+            "assistant": "assistant",
+            "gpt": "assistant",
+            "gpt-chat": "assistant",
+        }
+
+        response_prefill = prompt.get("response_prefill"):
+        if response_prefill is not None and response_prefill.strip() != "":
+            prompt["prompt"][-1]["prefill"] = response_prefill
+
+        turn_segments = []
+        token_count = 0
+        for i, turn in enumerate(prompt["prompt"]):
+            try:
+                sharegpt_value = turn.get("value")
+            except Exception as e:
+                LOG.warning(f"Processed sample will return empty due to: {e}")
+                return {"input_ids": [], "attention_mask": [], "labels": []}
+
+            # Skip empty turns
+            if sharegpt_value is None or sharegpt_value.strip() == "":
+                continue
+
+            prefix_text = (
+                f"<|start_header_id|>{role_dict[turn['from']]}<|end_header_id|>\n\n"
+            )
+
+            # All turns except the final turn
+            if i != len(prompt["prompt"]) - 1:
+                # Use non-regex tokenizer cause its faster. Only final turn needs regex
+                tokenized_text = self.tokenizer(
+                    text=f"{prefix_text}{sharegpt_value}<|eot_id|>",
+                    add_special_tokens=False,
+                    truncation=False,
+                    padding=False,
+                    return_tensors=None,
+                    return_offsets_mapping=True,
+                )
+
+                # Skip processing early if over token limit
+                token_count += len(tokenized_text["input_ids"])
+                if token_count >= self.sequence_len:
+                    return {"input_ids": [], "attention_mask": [], "labels": []}
+
+                # Add masked turn to turn segments
+                turn_segments.append(
+                    {
+                        "input_ids": tokenized_text["input_ids"],
+                        "attention_mask": tokenized_text["attention_mask"],
+                        "labels": [IGNORE_TOKEN_ID] * len(tokenized_text["input_ids"]),
+                    }
+                )
+            # Final turn
+            else:
+                # Add prefill to prefix_text if it exists
+                prefill_text = turn.get("prefill")
+                if prefill_text is not None and prefill_text.strip() != "":
+                    prefix_text += prefill_text
+
+                # Tokenize and create mask out undesired tokens using regex patterns
+                tokenized_text, regex_labels = regex_attention_tokenizer(
+                    tokenizer=self.tokenizer,
+                    text=f"{prefix_text}{sharegpt_value}<|eot_id|>",
+                )
+
+                # Skip processing early if over token limit
+                token_count += len(tokenized_text["input_ids"])
+                if token_count >= self.sequence_len:
+                    return {"input_ids": [], "attention_mask": [], "labels": []}
+
+                prefix_token_count = 0
+                for start, end in tokenized_text["offset_mapping"]:
+                    if end <= len(prefix_text):
+                        prefix_token_count += 1
+                    else:
+                        break
+
+                # Add partially masked turn to turn segments
+                turn_segments.append(
+                    {
+                        "input_ids": tokenized_text["input_ids"],
+                        "attention_mask": tokenized_text["attention_mask"],
+                        "labels": (
+                            [IGNORE_TOKEN_ID] * prefix_token_count  # Mask the prefix
+                            + regex_labels[prefix_token_count:]
+                        ),
+                    }
+                )
+
+        # Combine all the turn segments
+        input_ids, attention_mask, labels = [], [], []
+        for turn_segment in turn_segments:
+            input_ids.extend(turn_segment["input_ids"])
+            attention_mask.extend(turn_segment["attention_mask"])
+            labels.extend(turn_segment["labels"])
+
+        # Add missing BOS token if needed
+        if (
+            add_bos
+            and self.tokenizer.bos_token_id
+            and input_ids[0] != self.tokenizer.bos_token_id
+        ):
+            input_ids.insert(0, self.tokenizer.bos_token_id)
+            attention_mask.insert(0, 1)
+            labels.insert(0, IGNORE_TOKEN_ID)
+
+        # Training on samples with all tokens masked is a waste of compute
+        # May be worth checking if less than X% of tokens are trainable too
+        if all(label == IGNORE_TOKEN_ID for label in labels):
+            LOG.warning(
+                f"Processed sample will return empty due to no trainable tokens after masking"
+            )
+            return {"input_ids": [], "attention_mask": [], "labels": []}
+
+        return {
+            "input_ids": input_ids,
+            "attention_mask": attention_mask,
+            "labels": labels,
+        }
+
+
+# Function to load the CustomLLaMa3PromptTokenizingStrategy
+def load(tokenizer, cfg):
+    return CustomLLaMa3PromptTokenizingStrategy(
+        None, tokenizer, cfg.train_on_inputs, cfg.sequence_len
+    )
