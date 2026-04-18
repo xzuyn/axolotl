74269b54df1b9aaa582ff4593268a8e7c41f7b83
xzuyn <16216325+xzuyn@users.noreply.github.com>
2026-04-18 02:52:14 -0400

    Update customcompletion-regex.py

--- src/axolotl/prompt_strategies/customcompletion-regex.py
+++ src/axolotl/prompt_strategies/customcompletion-regex.py
@@ -44,55 +44,46 @@ class CustomCompletionPromptTokenizingStrategy(PromptTokenizingStrategy):
         self.field = "text" if not field else field
 
     def tokenize_prompt(self, prompt):
-        # Some tokenizers don't contain this, so if it doesn't exist assume it is set to True
-        add_bos = getattr(self.tokenizer, "add_bos_token", True)
-
-        # Tokenize and create mask out undesired tokens using regex patterns
         try:
             tokenized_text, regex_labels = regex_attention_tokenizer(
                 tokenizer=self.tokenizer,
                 text=ftfy.fix_text(prompt[self.field]).strip(),
+                add_special_tokens=True,
             )
-        except AttributeError:
-            LOG.warning(f"Processed sample will return empty due to AttributeError")
+
+            # Mask BOS token
+            if self.tokenizer.bos_token_id and tokenized_text["input_ids"][0] == self.tokenizer.bos_token_id:
+                regex_labels[0] = IGNORE_TOKEN_ID
+
+            # Add missing EOS token
+            if tokenized_text["input_ids"][-1] != self.tokenizer.eos_token_id:
+                tokenized_text["input_ids"].append(self.tokenizer.eos_token_id)
+                tokenized_text["attention_mask"].append(1)
+                regex_labels.append(self.tokenizer.eos_token_id)
+
+            # Training on samples with all tokens masked is a waste of compute
+            # May be worth checking if less than X% of tokens are trainable too
+            if all(label == IGNORE_TOKEN_ID for label in regex_labels):
+                LOG.warning(
+                    f"Processed sample will return empty due to no trainable tokens after masking"
+                )
+                return {"input_ids": [], "attention_mask": [], "labels": []}
+
+            if len(tokenized_text["input_ids"]) <= self.sequence_len:
+                return {
+                    "input_ids": tokenized_text["input_ids"],
+                    "attention_mask": tokenized_text["attention_mask"],
+                    "labels": regex_labels,
+                }
+            else:
+                return {
+                    "input_ids": tokenized_text["input_ids"][:self.sequence_len],
+                    "attention_mask": tokenized_text["attention_mask"][:self.sequence_len],
+                    "labels": regex_labels[:self.sequence_len],
+                }
+        except Exception as e:
+            LOG.warning(e)
             return {"input_ids": [], "attention_mask": [], "labels": []}
-
-        # Add missing BOS token
-        if (
-            add_bos
-            and self.tokenizer.bos_token_id
-            and tokenized_text["input_ids"][0] != self.tokenizer.bos_token_id
-        ):
-            tokenized_text["input_ids"].insert(0, self.tokenizer.bos_token_id)
-            tokenized_text["attention_mask"].insert(0, 1)
-            regex_labels.insert(0, IGNORE_TOKEN_ID)
-
-        # Add missing EOS token
-        if tokenized_text["input_ids"][-1] != self.tokenizer.eos_token_id:
-            tokenized_text["input_ids"].append(self.tokenizer.eos_token_id)
-            tokenized_text["attention_mask"].append(1)
-            regex_labels.append(self.tokenizer.eos_token_id)
-
-        # Training on samples with all tokens masked is a waste of compute
-        # May be worth checking if less than X% of tokens are trainable too
-        if all(label == IGNORE_TOKEN_ID for label in regex_labels):
-            LOG.warning(
-                f"Processed sample will return empty due to no trainable tokens after masking"
-            )
-            return {"input_ids": [], "attention_mask": [], "labels": []}
-
-        if len(tokenized_text["input_ids"]) <= self.sequence_len:
-            return {
-                "input_ids": tokenized_text["input_ids"],
-                "attention_mask": tokenized_text["attention_mask"],
-                "labels": regex_labels,
-            }
-        else:
-            return {
-                "input_ids": tokenized_text["input_ids"][: self.sequence_len],
-                "attention_mask": tokenized_text["attention_mask"][: self.sequence_len],
-                "labels": regex_labels[: self.sequence_len],
-            }
 
 
 # Function to load the CustomCompletionPromptTokenizingStrategy
