2e32aafd04f3e3e3e1c100c2be84765db9f77402
xzuyn <16216325+xzuyn@users.noreply.github.com>
2026-04-18 02:57:17 -0400

    Add files via upload

---
+++ src/axolotl/prompt_strategies/customcompletion.py
@@ -0,0 +1,94 @@
+"""Module containing the CustomCompletionPromptTokenizingStrategy class"""
+
+try:
+    import ftfy
+except ImportError:
+    raise ImportError("You need ftfy. https://pypi.org/project/ftfy/")
+import logging
+
+# Import from axolotl package
+from axolotl.prompt_tokenizers import PromptTokenizingStrategy
+
+try:
+    from axolotl.prompt_strategies.regex_attention import regex_attention_tokenizer
+except ImportError:
+    raise ImportError(
+        "You need https://github.com/xzuyn/axolotl/blob/latest-formatters/src/axolotl/prompt_strategies/regex_attention.py"
+    )
+
+
+# Set up logging
+LOG = logging.getLogger("axolotl")
+
+# Define a constant token ID to ignore
+IGNORE_TOKEN_ID = -100
+
+
+class CustomCompletionPromptTokenizingStrategy(PromptTokenizingStrategy):
+    """
+    Tokenizing strategy for CustomCompletion.
+    """
+
+    def __init__(self, prompter, tokenizer, field, sequence_len, *args, **kwargs):
+        # Call the superclass' constructor
+        super().__init__(
+            prompter=prompter,
+            tokenizer=tokenizer,
+            sequence_len=sequence_len,
+            *args,
+            **kwargs,
+        )
+        self.field = "text" if not field else field
+
+    def tokenize_prompt(self, prompt):
+        try:
+            tokenized_text = self.tokenizer(
+                text=ftfy.fix_text(prompt[self.field]).strip(),
+                add_special_tokens=True,
+                truncation=False,
+                padding=False,
+                return_tensors=None,
+                return_offsets_mapping=True,
+            )
+            labels = tokenized_text["input_ids"]
+
+            # Mask BOS token
+            if self.tokenizer.bos_token_id and tokenized_text["input_ids"][0] == self.tokenizer.bos_token_id:
+                labels[0] = IGNORE_TOKEN_ID
+
+            # Add missing EOS token
+            if tokenized_text["input_ids"][-1] != self.tokenizer.eos_token_id:
+                tokenized_text["input_ids"].append(self.tokenizer.eos_token_id)
+                tokenized_text["attention_mask"].append(1)
+                labels.append(self.tokenizer.eos_token_id)
+
+            # Training on samples with all tokens masked is a waste of compute
+            # May be worth checking if less than X% of tokens are trainable too
+            if all(label == IGNORE_TOKEN_ID for label in labels):
+                LOG.warning(
+                    f"Processed sample will return empty due to no trainable tokens after masking"
+                )
+                return {"input_ids": [], "attention_mask": [], "labels": []}
+
+            if len(tokenized_text["input_ids"]) <= self.sequence_len:
+                return {
+                    "input_ids": tokenized_text["input_ids"],
+                    "attention_mask": tokenized_text["attention_mask"],
+                    "labels": labels,
+                }
+            else:
+                return {
+                    "input_ids": tokenized_text["input_ids"][:self.sequence_len],
+                    "attention_mask": tokenized_text["attention_mask"][:self.sequence_len],
+                    "labels": labels[:self.sequence_len],
+                }
+        except Exception as e:
+            LOG.warning(e)
+            return {"input_ids": [], "attention_mask": [], "labels": []}
+
+
+# Function to load the CustomCompletionPromptTokenizingStrategy
+def load(tokenizer, cfg, ds_cfg):
+    return CustomCompletionPromptTokenizingStrategy(
+        None, tokenizer, ds_cfg.field, cfg.sequence_len
+    )
