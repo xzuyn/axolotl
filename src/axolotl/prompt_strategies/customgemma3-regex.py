c3c9a75201ae6bf72fc764bc38880b557f024a66
xzuyn <16216325+xzuyn@users.noreply.github.com>
2026-04-19 00:08:25 -0400

    Add files via upload

--- src/axolotl/prompt_strategies/customgemma3-regex.py
+++ src/axolotl/prompt_strategies/customgemma3-regex.py
@@ -1,8 +1,5 @@
 """Module containing the CustomGemma3PromptTokenizingStrategy class"""
 
-# Import necessary modules and functions
-import re
-
 try:
     import ftfy
 except ImportError:
@@ -46,171 +43,172 @@ class CustomGemma3PromptTokenizingStrategy(PromptTokenizingStrategy):
         )
 
     def tokenize_prompt(self, prompt):
-        # Some tokenizers don't contain this, so if it doesn't exist assume it is set to True
-        add_bos = getattr(self.tokenizer, "add_bos_token", True)
+        try:
+            # Some tokenizers don't contain this, so if it doesn't exist assume it is set to True
+            # add_bos = getattr(self.tokenizer, "add_bos_token", True)
+            add_bos = True
 
-        # ShareGPT-to-Gemma3 Dictionary
-        role_dict = {
-            "system": "system",
-            "human": "user",
-            "gpt": "model",
-            # Extra
-            "human-chat": "user",
-            "gpt-chat": "model",
-            # OpenAI/messages
-            "user": "user",
-            "assistant": "model",
-        }
+            # ShareGPT-to-Gemma3 Dictionary
+            role_dict = {
+                "system": "system",
+                "human": "user",
+                "gpt": "model",
+                # Extra
+                "human-chat": "user",
+                "gpt-chat": "model",
+                # OpenAI/messages
+                "user": "user",
+                "assistant": "model",
+            }
 
-        if "conversations" in prompt:
-            conversation_name = "conversations"
-            from_name = "from"
-            value_name = "value"
-        elif "conversation" in prompt:
-            conversation_name = "conversation"
-            from_name = "from"
-            value_name = "value"
-        elif "messages" in prompt:
-            conversation_name = "messages"
-            from_name = "role"
-            value_name = "content"
-        else:
-            LOG.warning(
-                f"sample does not contain 'conversations' or 'conversation' or 'messages'"
-            )
-            exit()
+            if "conversations" in prompt:
+                conversation_name = "conversations"
+                from_name = "from"
+                value_name = "value"
+            elif "conversation" in prompt:
+                conversation_name = "conversation"
+                from_name = "from"
+                value_name = "value"
+            elif "messages" in prompt:
+                conversation_name = "messages"
+                from_name = "role"
+                value_name = "content"
+            else:
+                LOG.warning(
+                    f"sample does not contain 'conversations' or 'conversation' or 'messages'"
+                )
+                exit()
 
-        # Iterate over each conversation turn in the prompt
-        turn_segments = []
-        for i, turn in enumerate(prompt[conversation_name]):
-            try:
+            # Iterate over each conversation turn in the prompt
+            turn_segments = []
+            for i, turn in enumerate(prompt[conversation_name]):
                 if turn[from_name] in ["human-chat", "gpt-chat"]:
-                    sharegpt_value = (
+                    sharegpt_value = ftfy.fix_text(
                         f"{turn['name'].strip()}: {turn[value_name].strip()}"
                     )
                 else:
-                    sharegpt_value = turn[value_name].strip()
-            except AttributeError:
-                LOG.warning(f"Processed sample will return empty due to AttributeError")
-                return {"input_ids": [], "attention_mask": [], "labels": []}
+                    sharegpt_value = ftfy.fix_text(turn[value_name].strip())
 
-            # Get string which will be masked out if using train_on_inputs: false
-            prefix_text = (
-                "\n" if i != 0 else ""
-            ) + f"<start_of_turn>{role_dict[turn['from']]}\n"
+                # Get string which will be masked out if using train_on_inputs: false
+                prefix_text = (
+                    "\n" if i != 0 else ""
+                ) + f"<start_of_turn>{role_dict[turn[from_name]]}\n"
 
-            # Tokenize and create mask out undesired tokens using regex patterns
-            tokenized_text, regex_labels = regex_attention_tokenizer(
-                tokenizer=self.tokenizer,
-                text=f"{prefix_text}{ftfy.fix_text(sharegpt_value).strip()}<end_of_turn>",
-            )
-
-            # Handle masked user turn
-            if self.train_on_inputs is False and turn[from_name] in [
-                "system",
-                "user",
-                "human",
-                "human-chat",
-            ]:
-                turn_segments.append(
-                    {
-                        from_name: turn[from_name],
-                        "input_ids": tokenized_text["input_ids"],
-                        "attention_mask": tokenized_text["attention_mask"],
-                        "labels": [IGNORE_TOKEN_ID] * len(regex_labels),
-                    }
+                # Tokenize and create mask out undesired tokens using regex patterns
+                tokenized_text, regex_labels = regex_attention_tokenizer(
+                    tokenizer=self.tokenizer,
+                    text=f"{prefix_text}{sharegpt_value}<end_of_turn>",
                 )
-            # Handle partially masked model turn
-            elif self.train_on_inputs is False and turn[from_name] in [
+
+                # Handle masked user turn
+                if self.train_on_inputs is False and turn[from_name] in [
+                    "system",
+                    "user",
+                    "human",
+                    "human-chat",
+                ]:
+                    turn_segments.append(
+                        {
+                            from_name: turn[from_name],
+                            "input_ids": tokenized_text["input_ids"],
+                            "attention_mask": tokenized_text["attention_mask"],
+                            "labels": [IGNORE_TOKEN_ID] * len(regex_labels),
+                        }
+                    )
+                # Handle partially masked model turn
+                elif self.train_on_inputs is False and turn[from_name] in [
+                    "assistant",
+                    "gpt",
+                    "gpt-chat",
+                ]:
+                    prefix_token_count = 0
+                    for start, end in tokenized_text["offset_mapping"]:
+                        if end <= len(prefix_text):
+                            prefix_token_count += 1
+                        else:
+                            break
+
+                    turn_segments.append(
+                        {
+                            from_name: turn[from_name],
+                            "input_ids": tokenized_text["input_ids"],
+                            "attention_mask": tokenized_text["attention_mask"],
+                            "labels": (
+                                [IGNORE_TOKEN_ID] * prefix_token_count  # Mask the prefix
+                                + regex_labels[prefix_token_count:]
+                            ),
+                        }
+                    )
+                # Handle unmasked turn
+                else:
+                    turn_segments.append(
+                        {
+                            from_name: turn[from_name],
+                            "input_ids": tokenized_text["input_ids"],
+                            "attention_mask": tokenized_text["attention_mask"],
+                            "labels": regex_labels,
+                        }
+                    )
+
+            # Only keep turns which add up to less than sequence_len (or seq_len - 1 if bos is set)
+            current_length = 0
+            trimmed_turn_segments = []
+            for turn_segment in turn_segments:
+                turn_segment_length = len(turn_segment["input_ids"])
+                if current_length + turn_segment_length > self.sequence_len - (
+                    1 if add_bos and self.tokenizer.bos_token_id else 0
+                ):
+                    break
+                else:
+                    trimmed_turn_segments.append(turn_segment)
+                    current_length += turn_segment_length
+
+            # Ensure the final turn is from gpt or gpt-chat
+            while trimmed_turn_segments and trimmed_turn_segments[-1][from_name] not in [
                 "assistant",
                 "gpt",
                 "gpt-chat",
             ]:
-                prefix_token_count = 0
-                for start, end in tokenized_text["offset_mapping"]:
-                    if end <= len(prefix_text):
-                        prefix_token_count += 1
-                    else:
-                        break
+                trimmed_turn_segments.pop()
 
-                turn_segments.append(
-                    {
-                        from_name: turn[from_name],
-                        "input_ids": tokenized_text["input_ids"],
-                        "attention_mask": tokenized_text["attention_mask"],
-                        "labels": (
-                            [IGNORE_TOKEN_ID] * prefix_token_count  # Mask the prefix
-                            + regex_labels[prefix_token_count:]
-                        ),
-                    }
-                )
-            # Handle unmasked turn
-            else:
-                turn_segments.append(
-                    {
-                        from_name: turn[from_name],
-                        "input_ids": tokenized_text["input_ids"],
-                        "attention_mask": tokenized_text["attention_mask"],
-                        "labels": regex_labels,
-                    }
-                )
+            # Return empty if there are less than 2 turns left
+            if len(trimmed_turn_segments) < 2:
+                # LOG.warning(f"Processed sample will return empty due to not enough turns")  # This spams
+                return {"input_ids": [], "attention_mask": [], "labels": []}
 
-        # Only keep turns which add up to less than sequence_len (or seq_len - 1 if bos is set)
-        current_length = 0
-        trimmed_turn_segments = []
-        for turn_segment in turn_segments:
-            turn_segment_length = len(turn_segment["input_ids"])
-            if current_length + turn_segment_length > self.sequence_len - (
-                1 if add_bos and self.tokenizer.bos_token_id else 0
+            # Combine all the turn segments
+            input_ids, attention_mask, labels = [], [], []
+            for turn_segment in trimmed_turn_segments:
+                input_ids.extend(turn_segment["input_ids"])
+                attention_mask.extend(turn_segment["attention_mask"])
+                labels.extend(turn_segment["labels"])
+
+            # Add missing BOS token if needed
+            if (
+                add_bos
+                and self.tokenizer.bos_token_id
+                and input_ids[0] != self.tokenizer.bos_token_id
             ):
-                break
-            else:
-                trimmed_turn_segments.append(turn_segment)
-                current_length += turn_segment_length
+                input_ids.insert(0, self.tokenizer.bos_token_id)
+                attention_mask.insert(0, 1)
+                labels.insert(0, IGNORE_TOKEN_ID)
 
-        # Ensure the final turn is from gpt or gpt-chat
-        while trimmed_turn_segments and trimmed_turn_segments[-1][from_name] not in [
-            "assistant",
-            "gpt",
-            "gpt-chat",
-        ]:
-            trimmed_turn_segments.pop()
+            # Training on samples with all tokens masked is a waste of compute
+            # May be worth checking if less than X% of tokens are trainable too
+            if all(label == IGNORE_TOKEN_ID for label in labels):
+                LOG.warning(
+                    f"Processed sample will return empty due to no trainable tokens after masking"
+                )
+                return {"input_ids": [], "attention_mask": [], "labels": []}
 
-        # Return empty if there are less than 2 turns left
-        if len(trimmed_turn_segments) < 2:
-            # LOG.warning(f"Processed sample will return empty due to not enough turns")  # This spams
+            return {
+                "input_ids": input_ids,
+                "attention_mask": attention_mask,
+                "labels": labels,
+            }
+        except Exception as e:
+            LOG.warning(e)
             return {"input_ids": [], "attention_mask": [], "labels": []}
-
-        # Combine all the turn segments
-        input_ids, attention_mask, labels = [], [], []
-        for turn_segment in trimmed_turn_segments:
-            input_ids.extend(turn_segment["input_ids"])
-            attention_mask.extend(turn_segment["attention_mask"])
-            labels.extend(turn_segment["labels"])
-
-        # Add missing BOS token if needed
-        if (
-            add_bos
-            and self.tokenizer.bos_token_id
-            and input_ids[0] != self.tokenizer.bos_token_id
-        ):
-            input_ids.insert(0, self.tokenizer.bos_token_id)
-            attention_mask.insert(0, 1)
-            labels.insert(0, IGNORE_TOKEN_ID)
-
-        # Training on samples with all tokens masked is a waste of compute
-        # May be worth checking if less than X% of tokens are trainable too
-        if all(label == IGNORE_TOKEN_ID for label in labels):
-            LOG.warning(
-                f"Processed sample will return empty due to no trainable tokens after masking"
-            )
-            return {"input_ids": [], "attention_mask": [], "labels": []}
-
-        return {
-            "input_ids": input_ids,
-            "attention_mask": attention_mask,
-            "labels": labels,
-        }
 
 
 # Function to load the CustomGemma3PromptTokenizingStrategy
--- src/axolotl/prompt_strategies/customgemma4-26b-31b.py
+++ src/axolotl/prompt_strategies/customgemma4-26b-31b.py
@@ -1,8 +1,5 @@
 """Module containing the CustomGemma4PromptTokenizingStrategy class"""
 
-# Import necessary modules and functions
-import re
-
 try:
     import ftfy
 except ImportError:
@@ -39,157 +36,157 @@ class CustomGemma4PromptTokenizingStrategy(PromptTokenizingStrategy):
         )
 
     def tokenize_prompt(self, prompt):
-        # Some tokenizers don't contain this, so if it doesn't exist assume it is set to True
-        # add_bos = getattr(self.tokenizer, "add_bos_token", True)
-        add_bos = True
+        try:
+            # Some tokenizers don't contain this, so if it doesn't exist assume it is set to True
+            # add_bos = getattr(self.tokenizer, "add_bos_token", True)
+            add_bos = True
 
-        # ShareGPT-to-ChatML Dictionary
-        role_dict = {
-            "system": "system",
-            "human": "user",
-            "gpt": "model",
-            # Extra
-            "human-chat": "user",
-            "gpt-chat": "model",
-            # OpenAI/messages
-            "user": "user",
-            "assistant": "model",
-        }
+            # ShareGPT-to-Gemma4 Dictionary
+            role_dict = {
+                "system": "system",
+                "human": "user",
+                "gpt": "model",
+                # Extra
+                "human-chat": "user",
+                "gpt-chat": "model",
+                # OpenAI/messages
+                "user": "user",
+                "assistant": "model",
+            }
 
-        if "conversations" in prompt:
-            conversation_name = "conversations"
-            from_name = "from"
-            value_name = "value"
-        elif "conversation" in prompt:
-            conversation_name = "conversation"
-            from_name = "from"
-            value_name = "value"
-        elif "messages" in prompt:
-            conversation_name = "messages"
-            from_name = "role"
-            value_name = "content"
-        else:
-            LOG.warning(
-                f"sample does not contain 'conversations' or 'conversation' or 'messages'"
-            )
-            exit()
+            if "conversations" in prompt:
+                conversation_name = "conversations"
+                from_name = "from"
+                value_name = "value"
+            elif "conversation" in prompt:
+                conversation_name = "conversation"
+                from_name = "from"
+                value_name = "value"
+            elif "messages" in prompt:
+                conversation_name = "messages"
+                from_name = "role"
+                value_name = "content"
+            else:
+                LOG.warning(
+                    f"sample does not contain 'conversations' or 'conversation' or 'messages'"
+                )
+                exit()
 
-        # Iterate over each conversation turn in the prompt
-        turn_segments = []
-        for i, turn in enumerate(prompt[conversation_name]):
-            try:
+            # Iterate over each conversation turn in the prompt
+            turn_segments = []
+            for i, turn in enumerate(prompt[conversation_name]):
                 if turn[from_name] in ["human-chat", "gpt-chat"]:
-                    sharegpt_value = (
+                    sharegpt_value = ftfy.fix_text(
                         f"{turn['name'].strip()}: {turn[value_name].strip()}"
                     )
                 else:
-                    sharegpt_value = turn[value_name].strip()
-            except AttributeError:
-                LOG.warning(f"Processed sample will return empty due to AttributeError")
+                    sharegpt_value = ftfy.fix_text(turn[value_name].strip())
+
+                # Get string which will be masked out if using train_on_inputs: false
+                # All turns except the final turn
+                if i != len(prompt[conversation_name]) - 1:
+                    prefix_text = (
+                        "\n" if i != 0 else ""
+                    ) + f"<|turn>{role_dict[turn[from_name]]}\n"
+                # Final turn
+                else:
+                    prefix_text = (
+                        "\n" if i != 0 else ""
+                    ) + f"<|turn>{role_dict[turn[from_name]]}\n<|channel>thought\n<channel|>"
+
+                # Tokenize
+                tokenized_text = self.tokenizer(
+                    text=f"{prefix_text}{sharegpt_value}<turn|>",
+                    add_special_tokens=False,
+                    truncation=False,
+                    padding=False,
+                    return_tensors=None,
+                    return_offsets_mapping=True,
+                )
+
+                # Handle masked user turn
+                if self.train_on_inputs is False and turn[from_name] in [
+                    "system",
+                    "user",
+                    "human",
+                    "human-chat",
+                ]:
+                    turn_segments.append(
+                        {
+                            from_name: turn[from_name],
+                            "input_ids": tokenized_text["input_ids"],
+                            "attention_mask": tokenized_text["attention_mask"],
+                            "labels": [IGNORE_TOKEN_ID] * len(tokenized_text["input_ids"]),
+                        }
+                    )
+                # Handle partially masked model turn
+                elif self.train_on_inputs is False and turn[from_name] in [
+                    "assistant",
+                    "gpt",
+                    "gpt-chat",
+                ]:
+                    prefix_token_count = 0
+                    for start, end in tokenized_text["offset_mapping"]:
+                        if end <= len(prefix_text):
+                            prefix_token_count += 1
+                        else:
+                            break
+
+                    turn_segments.append(
+                        {
+                            from_name: turn[from_name],
+                            "input_ids": tokenized_text["input_ids"],
+                            "attention_mask": tokenized_text["attention_mask"],
+                            "labels": (
+                                [IGNORE_TOKEN_ID] * prefix_token_count  # Mask the prefix
+                                + tokenized_text["input_ids"][prefix_token_count:]
+                            ),
+                        }
+                    )
+                # Handle unmasked turn
+                else:
+                    turn_segments.append(
+                        {
+                            from_name: turn[from_name],
+                            "input_ids": tokenized_text["input_ids"],
+                            "attention_mask": tokenized_text["attention_mask"],
+                            "labels": tokenized_text["input_ids"],
+                        }
+                    )
+
+            # Combine all the turn segments
+            input_ids, attention_mask, labels = [], [], []
+            for turn_segment in turn_segments:
+                input_ids.extend(turn_segment["input_ids"])
+                attention_mask.extend(turn_segment["attention_mask"])
+                labels.extend(turn_segment["labels"])
+
+            # Add missing BOS token if needed
+            if (
+                add_bos
+                and self.tokenizer.bos_token_id
+                and input_ids[0] != self.tokenizer.bos_token_id
+            ):
+                input_ids.insert(0, self.tokenizer.bos_token_id)
+                attention_mask.insert(0, 1)
+                labels.insert(0, IGNORE_TOKEN_ID)
+
+            # Training on samples with all tokens masked is a waste of compute
+            # May be worth checking if less than X% of tokens are trainable too
+            if all(label == IGNORE_TOKEN_ID for label in labels):
+                LOG.warning(
+                    f"Processed sample will return empty due to no trainable tokens after masking"
+                )
                 return {"input_ids": [], "attention_mask": [], "labels": []}
 
-            # Get string which will be masked out if using train_on_inputs: false
-            # All turns except the final turn
-            if i != len(prompt[conversation_name]) - 1:
-                prefix_text = (
-                    "\n" if i != 0 else ""
-                ) + f"<|turn>{role_dict[turn[from_name]]}\n"
-            # Final turn
-            else:
-                prefix_text = (
-                    "\n" if i != 0 else ""
-                ) + f"<|turn>{role_dict[turn[from_name]]}\n<|channel>thought\n<channel|>"
-
-            # Tokenize
-            tokenized_text = self.tokenizer(
-                text=f"{prefix_text}{ftfy.fix_text(sharegpt_value).strip()}<turn|>",
-                add_special_tokens=False,
-                truncation=False,
-                padding=False,
-                return_tensors=None,
-                return_offsets_mapping=True,
-            )
-
-            # Handle masked user turn
-            if self.train_on_inputs is False and turn[from_name] in [
-                "system",
-                "user",
-                "human",
-                "human-chat",
-            ]:
-                turn_segments.append(
-                    {
-                        from_name: turn[from_name],
-                        "input_ids": tokenized_text["input_ids"],
-                        "attention_mask": tokenized_text["attention_mask"],
-                        "labels": [IGNORE_TOKEN_ID] * len(tokenized_text["input_ids"]),
-                    }
-                )
-            # Handle partially masked model turn
-            elif self.train_on_inputs is False and turn[from_name] in [
-                "assistant",
-                "gpt",
-                "gpt-chat",
-            ]:
-                prefix_token_count = 0
-                for start, end in tokenized_text["offset_mapping"]:
-                    if end <= len(prefix_text):
-                        prefix_token_count += 1
-                    else:
-                        break
-
-                turn_segments.append(
-                    {
-                        from_name: turn[from_name],
-                        "input_ids": tokenized_text["input_ids"],
-                        "attention_mask": tokenized_text["attention_mask"],
-                        "labels": (
-                            [IGNORE_TOKEN_ID] * prefix_token_count  # Mask the prefix
-                            + tokenized_text["input_ids"][prefix_token_count:]
-                        ),
-                    }
-                )
-            # Handle unmasked turn
-            else:
-                turn_segments.append(
-                    {
-                        from_name: turn[from_name],
-                        "input_ids": tokenized_text["input_ids"],
-                        "attention_mask": tokenized_text["attention_mask"],
-                        "labels": tokenized_text["input_ids"],
-                    }
-                )
-
-        # Combine all the turn segments
-        input_ids, attention_mask, labels = [], [], []
-        for turn_segment in turn_segments:
-            input_ids.extend(turn_segment["input_ids"])
-            attention_mask.extend(turn_segment["attention_mask"])
-            labels.extend(turn_segment["labels"])
-
-        # Add missing BOS token if needed
-        if (
-            add_bos
-            and self.tokenizer.bos_token_id
-            and input_ids[0] != self.tokenizer.bos_token_id
-        ):
-            input_ids.insert(0, self.tokenizer.bos_token_id)
-            attention_mask.insert(0, 1)
-            labels.insert(0, IGNORE_TOKEN_ID)
-
-        # Training on samples with all tokens masked is a waste of compute
-        # May be worth checking if less than X% of tokens are trainable too
-        if all(label == IGNORE_TOKEN_ID for label in labels):
-            LOG.warning(
-                f"Processed sample will return empty due to no trainable tokens after masking"
-            )
+            return {
+                "input_ids": input_ids,
+                "attention_mask": attention_mask,
+                "labels": labels,
+            }
+        except Exception as e:
+            LOG.warning(e)
             return {"input_ids": [], "attention_mask": [], "labels": []}
-
-        return {
-            "input_ids": input_ids,
-            "attention_mask": attention_mask,
-            "labels": labels,
-        }
 
 
 # Function to load the CustomGemma4PromptTokenizingStrategy
