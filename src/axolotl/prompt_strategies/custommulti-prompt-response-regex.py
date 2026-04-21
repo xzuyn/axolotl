217651fb814b7b05ca92a4c6d70cd44bcacfce59
xzuyn <16216325+xzuyn@users.noreply.github.com>
2026-04-21 14:43:59 -0400

    Update custommulti-prompt-response-regex.py

--- src/axolotl/prompt_strategies/custommulti-prompt-response-regex.py
+++ src/axolotl/prompt_strategies/custommulti-prompt-response-regex.py
@@ -135,6 +135,20 @@ class CustomMultiPromptTokenizingStrategy(PromptTokenizingStrategy):
         full_text = f"{prefix_text}{content}{role_dict[role][1]}"
         return prefix_text, full_text
 
+    def handle_metharme(self, i, role, content):
+        role_dict = {
+            # ShareGPT
+            "system": "<|system|>",
+            "human": "<|user|>",
+            "gpt": "<|model|>",
+            # OpenAI/messages
+            "user": "<|user|>",
+            "assistant": "<|model|>",
+        }
+        prefix_text = role_dict[role][0]
+        full_text = f"{prefix_text}{content}</s>"
+        return prefix_text, full_text
+
     def tokenize_prompt(self, prompt):
         try:
             if self.tokenizer.bos_token_id is not None:
@@ -150,7 +164,7 @@ class CustomMultiPromptTokenizingStrategy(PromptTokenizingStrategy):
             random_handle = random.choice(
                 [
                     self.handle_chatml, self.handle_llama3, self.handle_gemma3,
-                    self.handle_gemma4, self.handle_fizzpaca, self.handle_mistral
+                    self.handle_gemma4, self.handle_fizzpaca, self.handle_mistral, self.handle_metharme
                 ]
             )
 
