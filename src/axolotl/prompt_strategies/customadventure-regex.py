a6ce4f83ca70704980894db077d918bec73af3ee
xzuyn <16216325+xzuyn@users.noreply.github.com>
2026-04-22 19:48:03 -0400

    Update customadventure-regex.py

--- src/axolotl/prompt_strategies/customadventure-regex.py
+++ src/axolotl/prompt_strategies/customadventure-regex.py
@@ -83,7 +83,9 @@ class CustomAdventurePromptTokenizingStrategy(PromptTokenizingStrategy):
                 if turn[from_name] == "system":
                     sharegpt_value = f"[{sharegpt_value}]"
                 elif turn[from_name] in ["user", "human"]:
-                    prefix_text += f"> "
+                    prefix_text += "> "
+                elif turn[from_name] in ["assistant", "gpt"] and i == len(prompt[conversation_name]) - 1:
+                    sharegpt_value += "\n> "
 
                 # Tokenize and create mask out undesired tokens using regex patterns
                 tokenized_text, regex_labels = regex_attention_tokenizer(
